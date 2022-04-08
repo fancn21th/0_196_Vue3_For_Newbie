b365cf9cac9ac7a1f0b3a9887b85f2ab6485b4ee
范轶洁 <fanyjb@digitalchina.com>
2022-04-08 11:52:02 +0800

    methods

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import * as Vue from "vue/dist/vue.esm-bundler.js";
 
 const app = Vue.createApp({
-  template: `<h1 class="text-3xl font-bold underline">{{message}}</h1>`,
+  template: `
+   <div>
+     <h1 class="text-3xl font-bold underline">{{message}}</h1>
+     <button class="bg-green-200 border-0 text-white p-6" @click="increment">Click</button>
+     <p>{{count}}</p>
+   </div>
+  `,
   data: () => ({
-    message: "Hello Vue3 & Vite",
+    count: 1,
   }),
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
 });
 
 app.mount("#app");
