8b24d770e1dc4544a5586709095fda71619a1f41
范轶洁 <fanyjb@digitalchina.com>
2022-04-08 12:42:36 +0800

    computed

--- src/index.js
+++ src/index.js
@@ -3,21 +3,30 @@ import * as Vue from "vue/dist/vue.esm-bundler.js";
 const app = Vue.createApp({
   template: `
    <div>
-     <h1 class="text-3xl font-bold underline">{{message}}</h1>
-     <button class="bg-green-200 border-0 text-white p-6" @click="increment">Click</button>
-     <p>{{count}}</p>
-     <p v-if="count % 2 === 0">你输入的是偶数</p>
-     <p v-else>你输入的是奇数</p>
+      <h1 class="text-3xl font-bold underline">{{message}}</h1>
+      <button class="bg-green-200 border-0 text-white p-6" @click="toggle">Click</button>
+      <ul>
+        <li v-for="number in evenNumbers" >{{number}}</li>
+      </ul>
    </div>
   `,
   data: () => ({
-    count: 1,
+    isEven: true,
+    numbers: Array.from(new Array(10).keys()),
   }),
   methods: {
-    increment() {
-      this.count++;
+    toggle() {
+      this.isEven = !this.isEven;
+    },
+  },
+  computed: {
+    evenNumbers() {
+      return this.numbers.filter(
+        (number) => (number % 2 === 0) === this.isEven
+      );
     },
   },
 });
 
 app.mount("#app");
+app.config.devtools = true;
