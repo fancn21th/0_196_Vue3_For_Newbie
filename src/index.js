d986c51e4b19c027a229e31d47b5fb5d4d767f69
范轶洁 <fanyjb@digitalchina.com>
2022-04-08 11:57:35 +0800

    v-if

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const app = Vue.createApp({
      <h1 class="text-3xl font-bold underline">{{message}}</h1>
      <button class="bg-green-200 border-0 text-white p-6" @click="increment">Click</button>
      <p>{{count}}</p>
+     <p v-if="count % 2 === 0">你输入的是偶数</p>
+     <p v-else>你输入的是奇数</p>
    </div>
   `,
   data: () => ({
