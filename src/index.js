eda907daa630c2e9caa05826c5cc4f2d91e38bfe
范轶洁 <fanyjb@digitalchina.com>
2022-04-08 11:16:36 +0800

    tailwind

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as Vue from "vue/dist/vue.esm-bundler.js";
 
 const app = Vue.createApp({
-  template: `<h1>{{message}}</h1>`,
+  template: `<h1 class="text-3xl font-bold underline">{{message}}</h1>`,
   data: () => ({
     message: "Hello Vue3 & Vite",
   }),
