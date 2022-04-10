02f1b4485f2c7a6bbe6fd9938a2e1781a6922137
范轶洁 <fanyjb@digitalchina.com>
2022-04-10 18:51:56 +0800

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <h2>{{ msg }}</h2>
+  <input
+    type="text"
+    v-model="msg"
+    class="shadow appearance-none border rounded w-100 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+  />
 </template>
 
 <script>
 // 显示地 开发模式
 // 更模块化
+import { ref } from "vue";
+
 export default {
   setup() {
-    const msg = "Hello Vue!";
+    const msg = ref("Hello Vue!");
     return {
       msg,
     };
