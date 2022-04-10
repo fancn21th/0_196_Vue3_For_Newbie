6b6f89ae58427ac9bd95578ebe7433e3be11e107
范轶洁 <fanyjb@digitalchina.com>
2022-04-10 19:48:22 +0800

    reactive

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <template>
-  <button
-    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-20"
-    @click="increment"
-  >
-    <!-- ref unwrapping -->
-    {{ count }}
-  </button>
+  <div>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded w-20"
+      @click="increment"
+    >
+      <!-- ref unwrapping -->
+      {{ count }}
+    </button>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded w-20"
+      @click="increase('foo')"
+    >
+      {{ numbers.foo }}
+    </button>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded w-20"
+      @click="increase('bar')"
+    >
+      {{ numbers.bar }}
+    </button>
+  </div>
 </template>
 
 <script>
 // 显示地 开发模式
 // 更模块化
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 export default {
   setup() {
+    // number, string
     const count = ref(0);
+    // {}
+    const numbers = reactive({
+      foo: 0,
+      bar: 0,
+    });
+    function increase(key) {
+      numbers[key]++;
+    }
     function increment() {
       // 修改 value 而不是 count
       console.log(count);
@@ -23,7 +46,9 @@ export default {
     }
     return {
       count,
+      numbers,
       increment,
+      increase,
     };
   },
   methods: {},
