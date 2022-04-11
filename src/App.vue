0c34c9b25c408b7f2a0cd9dcb74a8f4bf3c52f30
范轶洁 <fanyjb@digitalchina.com>
2022-04-11 16:36:24 +0800

    use-xxx

--- src/App.vue
+++ src/App.vue
@@ -30,24 +30,10 @@
 </template>
 
 <script>
-// 显示地 开发模式
-// 更模块化
-import { ref, reactive, computed, watch, watchEffect } from "vue";
+import { useNumbers } from "./useNumbers";
 
 export default {
   setup() {
-    // ref <--> number, string
-    const count = ref(0);
-    const history = ref([]);
-    // reactive <--> {}
-    const numbers = reactive({
-      foo: 0,
-      bar: 0,
-    });
-    // computed <--> computed(() => {})
-    const total = computed(() => {
-      return count.value + numbers.foo + numbers.bar;
-    });
     // methods
     function increase(key) {
       numbers[key]++;
@@ -57,41 +43,11 @@ export default {
       console.log(count);
       count.value++;
     }
-    // watch
-    watch(
-      numbers,
-      (newValue, oldValue) => {
-        if (oldValue && oldValue !== newValue) {
-          console.log(`foo: ${oldValue.foo} -> ${newValue.foo}`);
-          console.log(`bar: ${oldValue.bar} -> ${newValue.bar}`);
-        }
-      },
-      {
-        immediate: true,
-      }
-    );
-    watch(
-      [count],
-      ([newValue], [oldValue]) => {
-        if (oldValue !== newValue) {
-          history.value.push(`count: ${oldValue} -> ${newValue}`);
-        }
-      },
-      {
-        immediate: true,
-      }
-    );
-    // watchEffect
-    watchEffect(() => {
-      console.log(count.value);
-    });
+
     return {
-      count,
-      history,
-      numbers,
       increment,
       increase,
-      total,
+      ...useNumbers(),
     };
   },
   methods: {},
