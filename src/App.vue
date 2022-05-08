2ef5c5965a7f0ae58cdc06bd9b1e0c1397ad7e68
fancn21th <fancn21th@gmail.com>
2022-05-08 17:43:28 +0800

    setup function is executed once

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <h2>{{ msg }}</h2>
+  <input type="text" v-model="msg" />
 </template>
 
 <script>
 // 显示地 开发模式
 // 更模块化
 export default {
+  // 执行一次， 因此 msg 其实不会变
   setup() {
     const msg = "Hello Vue!";
     return {
