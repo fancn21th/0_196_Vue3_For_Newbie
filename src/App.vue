383aae4cff07a6e4bff8c5fec711b3790cf923b9
fancn21th <fancn21th@gmail.com>
2022-05-21 13:09:20 +0800

    tailwind setup

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,11 @@
 <template>
-  <div>
-    <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded w-20"
-      @click="increment"
-    >
-      <!-- ref unwrapping -->
-      {{ count }}
-    </button>
-    <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded w-20"
-      @click="increase('foo')"
-    >
-      {{ numbers.foo }}
-    </button>
-    <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded w-20"
-      @click="increase('bar')"
-    >
-      {{ numbers.bar }}
-    </button>
-
-    <p>Total :{{ total }}</p>
-    <ul>
-      <li v-for="his in history" :key="his">
-        {{ his }}
-      </li>
-    </ul>
-  </div>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
 <script>
-import { useNumbers } from "./useNumbers";
-
 export default {
   setup() {
-    const { count, history, numbers, total } = useNumbers();
-    // methods
-    function increase(key) {
-      numbers[key]++;
-    }
-    function increment() {
-      // 修改 value 而不是 count
-      console.log(count);
-      count.value++;
-    }
-
-    return {
-      increment,
-      increase,
-      count,
-      history,
-      numbers,
-      total,
-    };
+    return {};
   },
   methods: {},
 };
