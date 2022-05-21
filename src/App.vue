66af5f19984824a22ab06adf4f45aa5efb27492d
fancn21th <fancn21th@gmail.com>
2022-05-21 14:55:30 +0800

    基本样式

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div class="px-8 py-12 max-w-md mx-auto">
+    <img
+      src="https://images.unsplash.com/photo-1641261689141-ee46b8a0470c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370"
+      alt="todos"
+      class="h-32 rounded-lg shadow-xl"
+    />
+    <h1 class="mt-6 text-2xl font-bold text-gray-900 leading-tight">
+      What is your next <span class="text-indigo-400">TODOs</span>
+    </h1>
+    <p class="mt-2 text-gray-600">
+      A simple todo list app built with Vue.js and TailwindCSS.
+    </p>
+    <div class="mt-4">
+      <a
+        href="#"
+        class="inline-block bg-indigo-500 text-white px-5 py-3 rounded-lg shadow-lg uppercase tracking-wider font-semibold text-sm"
+        >Add another todo</a
+      >
+    </div>
+  </div>
 </template>
 
 <script>
