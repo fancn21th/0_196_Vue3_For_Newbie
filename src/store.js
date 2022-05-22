1bd3c5dfd4a5fb407d13deedfa1ea2e5f86f9a7c
fancn21th <fancn21th@gmail.com>
2022-05-22 19:36:41 +0800

    持久化

--- src/store.js
+++ src/store.js
@@ -1,28 +1,16 @@
 import { reactive } from "vue";
+import { useStorage } from "@vueuse/core";
 
 class Store {
   constructor() {
+    const todos = useStorage("fyj-todos", []);
+
     this.idx = 1;
     this.state = reactive({
-      todos: [
-        {
-          id: this.idx++,
-          title: "Learn Vue 3",
-          done: true,
-        },
-        {
-          id: this.idx++,
-          title: "Learn React",
-          done: false,
-        },
-        {
-          id: this.idx++,
-          title: "Learn Tailwind CSS",
-          done: true,
-        },
-      ],
+      todos,
     });
   }
+
   addTodo(title) {
     this.state.todos.push({
       id: this.idx++,
